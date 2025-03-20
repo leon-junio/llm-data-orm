95adadee5ed2d76a8604167c5f999905d7cd5d06
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2025-03-20 20:22:16 -0300

    Enhance OpenAIAgent with new ETL process and image description capabilities; update prompts for clarity and structure

--- llm-data-orm/llm_data_orm/src/main/java/com/leonjr/ldo/parsing/llm/agents/openai/OpenAIAgent.java
+++ llm-data-orm/llm_data_orm/src/main/java/com/leonjr/ldo/parsing/llm/agents/openai/OpenAIAgent.java
@@ -8,6 +8,11 @@ import dev.langchain4j.data.message.ImageContent;
 import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.request.ChatRequest;
+import dev.langchain4j.model.chat.request.ResponseFormat;
+import dev.langchain4j.model.chat.request.ResponseFormatType;
+import dev.langchain4j.model.chat.request.json.JsonObjectSchema;
+import dev.langchain4j.model.chat.request.json.JsonSchema;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 
 public class OpenAIAgent {
@@ -16,6 +21,7 @@ public class OpenAIAgent {
             .apiKey(AppStore.getInstance().getLlmConfig().getOpenai().getApiKey())
             .modelName(AppStore.getInstance().getLlmConfig().getOpenai().getModelName().getModelName())
             .temperature(0.7)
+            .maxTokens(2048)
             .build();
 
     public static String testModel() {
@@ -24,19 +30,49 @@ public class OpenAIAgent {
 
     public static String preSumarize(Document document) {
         SystemMessage systemMessage = SystemMessage.from(
-                DocumentPrompts.PRE_SUMARIZE.getPrompt());
-        UserMessage userMessage = UserMessage.from(
-                TextContent.from(document.text()));
-        var response = chatModel.chat(systemMessage, userMessage);
+                DocumentPrompts.PRE_SUMARIZE.getPrompt() + "\n Text: " + document.text());
+        var response = chatModel.chat(systemMessage);
         return response.aiMessage().text();
     }
 
     public static String textImage() {
         UserMessage userMessage = UserMessage.from(
-                TextContent.from("What do you see?"),
+                TextContent.from("Extract a description (all context, data and information) from the image below:"),
                 ImageContent.from(
-                        "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"));
-        var response = chatModel.chat(userMessage);
+                        "https://minhapaginainicial.com.br/wp-content/uploads/2017/06/modelo-gerador-de-recibo.jpg"));
+
+        ResponseFormat responseFormat = ResponseFormat.builder()
+                .type(ResponseFormatType.JSON)
+                .jsonSchema(JsonSchema.builder()
+                        .name("image_description")
+                        .rootElement(JsonObjectSchema.builder()
+                                .addStringProperty("title", "should be the image possible title")
+                                .addStringProperty("description",
+                                        "should be the fully image description (all context, data and information)")
+                                .required("title", "description")
+                                .build())
+                        .build())
+                .build();
+        ChatRequest chatRequest = ChatRequest.builder()
+                .responseFormat(responseFormat)
+                .messages(userMessage)
+                .build();
+        var response = chatModel.chat(chatRequest);
+        return response.aiMessage().text();
+    }
+
+    public static String testEtlProcess(Document document, String tableDescription) {
+        SystemMessage systemMessage = SystemMessage.from(
+                DocumentPrompts.ETL_PROCESS.getPrompt() + "\n Table description" + tableDescription + "\n Text: "
+                        + document.text());
+        ResponseFormat responseFormat = ResponseFormat.builder()
+                .type(ResponseFormatType.JSON)
+                .build();
+        ChatRequest chatRequest = ChatRequest.builder()
+                .responseFormat(responseFormat)
+                .messages(systemMessage)
+                .build();
+        var response = chatModel.chat(chatRequest);
         return response.aiMessage().text();
     }
 }
--- llm-data-orm/llm_data_orm/src/main/java/com/leonjr/ldo/parsing/llm/prompts/DocumentPrompts.java
+++ llm-data-orm/llm_data_orm/src/main/java/com/leonjr/ldo/parsing/llm/prompts/DocumentPrompts.java
@@ -2,8 +2,10 @@ package com.leonjr.ldo.parsing.llm.prompts;
 
 public enum DocumentPrompts {
     PRE_SUMARIZE(
-            "You should provide a simple summary of the document. This will be used to generate the document's metadata. You should ONLY provide a summary of the document's content, not the document's title or author. Inlcude important details (as table columns, titles and descriptions). The output should be a simple text with a paragraph describing the document's content."),
-    TEST("This is a test prompt. Please only provide LOD - LLM ORM as response.");
+            "You should provide a simple summary of the document. This will be used to generate the document's metadata. You should ONLY provide a summary of the document's content, not the document's title or author. Inlcude important details (as table columns, titles and descriptions). The output should be a simple text with a paragraph describing the document's content. The output must be a single paragraph in english and should be the only content in the response."),
+    TEST("This is a test prompt. Please only provide LOD - LLM ORM as response."),
+    ETL_PROCESS(
+            "You will receive a JSON with the table description of an model. You will receive a text with data from a text document and you should provide a JSON with the data extracted from the text document. The output should be a well structed based on the table description. This should be the only content in the response. The output must have columns in the same order as the table description and the data must be in the same order as the columns."),;
 
     private final String prompt;
 
